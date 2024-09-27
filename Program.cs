339d5bf3d51bab5ca7d0cdd6538b24cf4d3d9bd9
Загир Валитов <zagir-valitov@outlook.com>
2024-09-27 11:16:17 +0500

    Запись в файлы спомощью StreamWriter

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿Console.WriteLine(" ---- Home work 10 ----\n");
+﻿using System.Data;
+using System.Globalization;
+
+Console.WriteLine(" ---- Home work 10 ----\n");
 
 string directoryPath1 = @"D:\OTUS\TestDir1";
 DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath1);
-
 if (!directoryInfo.Exists)
 {
     directoryInfo.Create();
@@ -10,7 +12,6 @@ if (!directoryInfo.Exists)
 
 string directoryPath2 = @"D:\OTUS\TestDir2";
 directoryInfo = new DirectoryInfo(directoryPath2);
-
 if (!directoryInfo.Exists)
 {
     directoryInfo.Create();
@@ -25,31 +26,66 @@ for (int i = 1; i <= 10; i++)
     {
        File.Create(filePath).Dispose();
     }
-    /*
-    path = $@"D:\OTUS\TestDir1\File_{i}.txt";
+
+    filePath = $@"D:\OTUS\TestDir1\File_{i}.txt";
     string newPath = $@"D:\OTUS\TestDir2\File_{i}.txt";
-    fileInfo = new FileInfo(path);
+    fileInfo = new FileInfo(filePath);
 
     if (fileInfo.Exists)
     {
-        File.Copy(path, newPath, true);
-    }
-    */
+        File.Copy(filePath, newPath, true);
+    }    
 }
+
+
 
 if (Directory.Exists(directoryPath1))
 {
     var fileList = Directory.GetFiles(directoryPath1);
-    Console.WriteLine("Files: ");
+
+    for (int i =  0; i < fileList.Length; i++)
+    {
+        using (StreamWriter writer = new StreamWriter(fileList[i], false))
+        { 
+            await writer.WriteLineAsync(new FileInfo(fileList[i]).Name);
+        }
+
+        using (StreamWriter writer = new StreamWriter(fileList[i], true))
+        {
+            await writer.WriteLineAsync(Convert.ToString(DateTime.Now));
+        }
+    }
+
+    /*
+    Console.WriteLine($"Files: {fileList.Length}\n");
+
     List<string> fileNameList = new();
     foreach (var file in fileList)
     {
         FileInfo fileNameInfo = new(file);
         fileNameList.Add(fileNameInfo.Name);
-        Console.WriteLine(fileNameInfo.Name);
+        Console.WriteLine(fileNameInfo);
+
     }
+    */
 }
 
+if (Directory.Exists(directoryPath2))
+{
+    var fileList = Directory.GetFiles(directoryPath2);
 
+    for (int i = 0; i < fileList.Length; i++)
+    {
+        using (StreamWriter writer = new StreamWriter(fileList[i], false))
+        {
+            await writer.WriteLineAsync(new FileInfo(fileList[i]).Name);
+        }
+
+        using (StreamWriter writer = new StreamWriter(fileList[i], true))
+        {
+            await writer.WriteLineAsync(Convert.ToString(DateTime.Now));
+        }
+    }
+}
 
 
